75a8798074bf65e0de6dfe002364fd96c2a410be
Dmitrii Belash <iscincidae@gmail.com>
2022-12-29 13:38:21 +0200

    fixed a code

--- Challenge-1/solution.php
+++ Challenge-1/solution.php
@@ -11,3 +11,4 @@ echo "<h1>Hello, $name !</h1>\n";
 echo "<p>I was very happy to saw you in the $city. I was surprised that you liked our favourite $candy candy.</p>\n";
 echo "<p>Today I`m going to the cinema with my friend <b>$friend</b> to see a new <i>$movie</i> movie. Tickets are only \$6 dollars each!</p>\n";
 echo "<p>Hope to hear from you soon!\n Your friend, Mike.</p>";
+?>
