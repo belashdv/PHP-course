41bbc632d9bc1f1383b7d559dbf825e2730c0efd
Dmitrii Belash <iscincidae@gmail.com>
2022-12-29 13:37:43 +0200

    done a homework for the first challenge from couse

---
+++ Challenge-1/solution.php
@@ -0,0 +1,13 @@
+<?php
+// Write a letter about walk to the cinema using this variables:
+
+$name= "Teresa"; // Heading 1
+$city = "NYC";
+$movie = "Star Wars"; //Italics
+$friend = "George"; //Bold
+$candy = "Sour Patch Kids";
+
+echo "<h1>Hello, $name !</h1>\n";
+echo "<p>I was very happy to saw you in the $city. I was surprised that you liked our favourite $candy candy.</p>\n";
+echo "<p>Today I`m going to the cinema with my friend <b>$friend</b> to see a new <i>$movie</i> movie. Tickets are only \$6 dollars each!</p>\n";
+echo "<p>Hope to hear from you soon!\n Your friend, Mike.</p>";
